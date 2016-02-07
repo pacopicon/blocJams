95a765d2ea47522f03fc808834b7a30cadf04c16
Paco <palmtreerooskee@gmail.com>
2016-02-07 09:44:51 -0600

    assignment 23 complete

---
+++ scripts/utilities.js
@@ -0,0 +1,11 @@
+var forEach = function(elements, callback) {
+    for (var i = 0; i < elements.length; i++) {
+        callback(elements[i]);
+    }
+};
+
+//function = forEach(elements, callback) {
+//    for (var i = 0; i < elements.length; i++) {
+//        callback(elements[i]);
+//    }
+//};
